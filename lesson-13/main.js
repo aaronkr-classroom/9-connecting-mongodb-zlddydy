218e05d5af07234768a5cfceb8617a478f609a3e
zlddydy <75608752+zlddydy@users.noreply.github.com>
2024-05-01 05:41:01 +0000

    13 끝!!

--- lesson-13/main.js
+++ lesson-13/main.js
@@ -6,11 +6,42 @@ const port = 3000,
   layouts = require("express-ejs-layouts"),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  app = express();
+  app = express(),
+  MongoDB = require('mongodb').MongoClient,
+  dbURL = "mongodb+srv://Chou:n92KGQbSYPDngV9n@atlascluster.2xqcaun.mongodb.net/", // Atlas에서 생성
+  dbName = "ut-node";
   // @TODO: 몽고DB 모듈의 요청
 
-// @TODO: 로컬 MongoDB 데이터베이스 서버 연결 설정
+// @TODO: Atlas MongoDB 데이터베이스 서버 연결 설정
+MongoDB.connect(dbURL, (error, client) => {
+  if (error) throw error;
 
+  let db = client.db(dbName); // ut-node
+  db.collection("contacts")
+  .find()
+  .toArray((error, data) => {
+    if (error) throw error;
+    console.log(data);
+  });
+
+  db.collection("contacts")
+  .insertOne({
+    name: "Psy",
+    job: "singer",
+    from: "korea"
+  }, (error, result) => {
+    if (error) throw error;
+    console.log(result);
+  });
+})
+.then(() => {
+  console.log("DB connected!!!!!!!");
+})
+.catch((error) => {
+  console.log("DB connection FAILED!");
+  console.log(error);
+  process.exit(1);
+});
 
 app.set("port", process.env.PORT || port);
 app.set("view engine", "ejs");
