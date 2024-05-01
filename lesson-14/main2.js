d36c0de8feecffdd6ce6f1419e653e2de0c47570
zlddydy <75608752+zlddydy@users.noreply.github.com>
2024-05-01 06:18:54 +0000

    14끝!!

--- lesson-14/main2.js
+++ lesson-14/main2.js
@@ -6,35 +6,86 @@ const port = 3000,
   layouts = require("express-ejs-layouts"),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  // @TODO: Subscriber 모델 가져오기
+  Subscriber = require("./models/subscriber"),
   app = express();
 
 /**
  * @TODO: Listing 14.1 (p. 205)
  * Mongoose를 사용한 MongoDB 연결
  */
+const mongoose = require("mongoose"),
+dbURL = "mongodb+srv://Chou:n92KGQbSYPDngV9n@atlascluster.2xqcaun.mongodb.net", // Atlas에서 생성
+dbName = "ut-node";
 
+mongoose.connect(
+  dbURL + "/" + dbName, // URL + DBname
+  { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
 
 /**
  * @TODO: Listing 14.2 (p. 206)
  * 데이터베이스 연결 이벤트 처리
  */
-
+db.once("open", () => {
+  console.log("DB connected!!!!!");
+});
 
 /**
  * Listing 14.4 (p. 207)
  * 생성과 저장 구문
  */
 
+// version1
+let sub1 = new Subscriber({
+  name: "Aaron",
+  email: "zlddydy@naver.com",
+  phone: 13124214144,
+  newsletter: true
+});
+// (택) DB find() after 14.5
+sub1.save()
+  .then(savedDoc => {
+    console.log(savedDoc);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-// (선택) DB find() after 14.5
-
-
+  // version2
+  Subscriber.create({
+    name: "Tom",
+    email: "affasfjnaer@av.com",
+    phone: 11111,
+    newsletter: false
+  })
+  .then(savedDoc => {
+    console.log(savedDoc);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 /**
  * @TODO: Listing 14.6 (p. 208)
  * 데이터베이스에서 데이터 검색
  */
+let tom = Subscriber
+            .findOne({name: "Tom"})
+            .where(
+              "email",
+              /loki/
+            );
+console.log("Found: ", tom);
 
+var query = Subscriber.findOne({
+  name: "Aaron",
+}).where("email", /aaron/);
+
+query.exec()
+    .then((error, data) => {
+      if (data) console.log(data.name);
+    });
 
 app.set("port", process.env.PORT || port);
 app.set("view engine", "ejs");
--- lesson-14/models/subscriber.js
+++ lesson-14/models/subscriber.js
@@ -5,6 +5,15 @@
  * @TODO: Listing 14.3 (p. 206)
  * 스키마 정의
  */
-
-
-// Subscriber 모델 생성
+const mongoose = require("mongoose"),
+    subscriberSchema = mongoose.Schema({
+        name: String,
+        email: String,
+        phone: Number,
+        newsletter: Boolean
+    });
+// Subscriber 모델 내보내기
+module.exports = mongoose.model(
+    "Subscriber",
+    subscriberSchema
+);
